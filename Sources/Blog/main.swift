13c31de79c7b81cae3ed1bd59df65225f2fa712e
Dave Verwer <dave.verwer@gmail.com>
2020-08-02 12:47:21 +0100

    Basic configuration.

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -2,10 +2,9 @@ import Foundation
 import Publish
 import Plot
 
-// This type acts as the configuration for your website.
 struct Blog: Website {
+
     enum SectionID: String, WebsiteSectionID {
-        // Add the sections that you want your website to contain here:
         case posts
     }
 
@@ -13,13 +12,14 @@ struct Blog: Website {
         // Add any site-specific metadata that you want to use here.
     }
 
-    // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
-    var name = "Blog"
-    var description = "A description of Blog"
+    var url = URL(string: "https://blog.swiftpackageindex.com")!
+    var name = "Swift Package Index Blog"
+    var description = """
+        The Swift Package Index is the place to find Swift packages.
+        The project is open-source and community driven, and this blog tells the story of its development.
+    """
     var language: Language { .english }
     var imagePath: Path? { nil }
 }
 
-// This will generate your website using the built-in Foundation theme:
 try Blog().publish(withTheme: .foundation)
