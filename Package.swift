25a3e152553e07c5a31bed5b7dccd375fa4edfd7
Dave Verwer <dave.verwer@gmail.com>
2020-08-15 18:42:07 +0100

    Switched Publish to depend on the master branch.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0")
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", .branch("master"))
     ],
     targets: [
         .target(
