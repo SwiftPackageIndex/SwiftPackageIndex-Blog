a888b03513e374ae42936aaca16345735c4075c8
Dave Verwer <dave.verwer@gmail.com>
2020-08-16 12:56:40 +0100

    Revert "Switched to deploy from (and to?) main rather than master."
    This reverts commit c02a2139a2e0ae3c36b965516f71bf2a9ddce987.

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -25,6 +25,6 @@ struct Blog: Website {
 try Blog().publish(
     withTheme: .spi,
     additionalSteps: [
-        .deploy(using: .gitHub("SwiftPackageIndex/SwiftPackageIndex.github.io", branch: "main"))
+        .deploy(using: .gitHub("SwiftPackageIndex/SwiftPackageIndex.github.io"))
     ]
 )
